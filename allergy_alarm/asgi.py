dbc293201cc28ec12f47d910db42267304b6691f
Triangleman1 <spnewman21@gmail.com>
2025-01-28 11:31:34 -0500

    Project Start

---
+++ allergy_alarm/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for allergy_alarm project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'allergy_alarm.settings')
+
+application = get_asgi_application()
---
+++ allergy_alarm_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AllergyAlarmAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'allergy_alarm_app'
