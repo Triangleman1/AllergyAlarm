c1a517f12062d8fd627c45241f4f0bf6621e073d
Triangleman1 <spnewman21@gmail.com>
2025-01-31 22:13:27 -0500

    CSS file now applies properly. Pretty sure the bug had to do with the file path in the html file.

--- allergy_alarm/urls.py
+++ allergy_alarm/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf.urls.static import static
+from django.conf import settings
 from allergy_alarm_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('home/', include("allergy_alarm_app.urls"))
     path("", views.home, name="home")
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
