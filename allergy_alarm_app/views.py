0d6f63b4fb1a7f68153a0ac14a74a45de83dca0a
unknown <ksharpe6@vols.utk.edu>
2025-02-10 00:18:36 -0500

    Fixed dashboard

--- allergy_alarm_app/views.py
+++ allergy_alarm_app/views.py
@@ -5,3 +5,6 @@ from allergy_alarm_app import templates
 # Create your views here.
 def home(request):
     return render(request, "allergy_alarm_app/allergy_home.html")
+
+def dashboard(request):
+    return render(request, "allergy_alarm_app/base.html")
