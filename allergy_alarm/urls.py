0a4ce8529209b0c48716dc8ac4139e144a71cba3
unknown <ksharpe6@vols.utk.edu>
2025-02-10 00:18:36 -0500

    Fixed dashboard

--- allergy_alarm/urls.py
+++ allergy_alarm/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -23,7 +23,6 @@ from allergy_alarm_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("dashboard", views.dashboard, name="dashboard")
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
