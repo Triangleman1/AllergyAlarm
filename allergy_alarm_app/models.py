99e963e338a12ca3bc7871fbbffdbde5a563e82a
Triangleman1 <spnewman21@gmail.com>
2025-03-06 21:33:51 -0500

    Initial SQL Basics

--- allergy_alarm_app/models.py
+++ allergy_alarm_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class UserData(models.Model):
+    datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
+    temperature = models.IntegerField()
+    heart_rate = models.IntegerField()
+
+    def __str__(self):
+        return f"Date/Time: {self.datetime}, temp: {self.temperature}, heart rate:{self.heart_rate}"
