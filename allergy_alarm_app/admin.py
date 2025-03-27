6201f376197b33e53f669bf76afe1dc89607bffa
Triangleman1 <spnewman21@gmail.com>
2025-03-27 12:02:03 -0400

    Update to database structure

--- allergy_alarm_app/admin.py
+++ allergy_alarm_app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import UserData
+from .models import UserIdentification, Temperature, HeartRate, Accelerometer, Gyroscope
 # Register your models here.
-admin.site.register(UserData)
+admin.site.register(UserIdentification)
+admin.site.register(Temperature)
+admin.site.register(HeartRate)
+admin.site.register(Accelerometer)
+admin.site.register(Gyroscope)
