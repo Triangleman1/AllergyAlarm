124af62b23e5d4bb6d99a1e3bcbb23002f4cadb2
Triangleman1 <spnewman21@gmail.com>
2025-04-15 11:38:47 -0400

    Four operational plots. Created infrastructure for passing button-specific variables to javascript and the view it fetches for plotting.

--- allergy_alarm_app/views.py
+++ allergy_alarm_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
@@ -7,7 +7,7 @@ import openpyxl
 import io
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from .models import Temperature
+from .models import *
 
 # Create your views here.
 def home(request):
@@ -28,57 +28,75 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("home"))
+            return redirect("home")
         else:
             return render(request, "allergy_alarm_app/login.html", {
                 "message": "Invalid credentials"
             })
     return render(request, "allergy_alarm_app/login.html")
 
-def logout(request):
+def logout_view(request):
+    logout(request)
     return render(request, "allergy_alarm_app/login.html", {
         "message": "Logged out."
     })
 
-def chart_view(request):
-    return render(request, 'allergy_alarm_app/chart.html')
+def chart_view(request, sensorType):
+    return render(request, 'allergy_alarm_app/chart.html', {
+                "sensorType": sensorType
+            })
 
-def chart_data(request):
-    data = Temperature.objects.all()
+def chart_data(request, sensorType):
+    #Called by chart.html, gives it the proper data. sensorType is a string which should match the sensor and table name
+    if (sensorType == "Temperature"): 
+        print("test:temp")
+        data = Temperature.objects.all()
+        values = [point.temperature for point in data]
+    elif (sensorType == "HeartRate"): 
+        data = HeartRate.objects.all()
+        values = [point.ECG for point in data]
+    elif (sensorType == "Accelerometer"): 
+        print("test:accel")
+        data = Accelerometer.objects.all()
+        values = [point.x for point in data]
+    elif (sensorType == "Gyroscope"): 
+        data = Gyroscope.objects.all()
+        values = [point.x for point in data]
     print([point.userID for point in data])
     labels = [point.datetime.strftime("%B %d, %I:%M%p") for point in data] #if point.user_id == user
-    values = [point.temperature for point in data]
     return JsonResponse(data={
         'labels': labels,
         'values': values,
+        'ylabel': sensorType,
     })
 
-def excel_to_chart(request):
-    # Your Excel file path (or it could be uploaded by the user in a form)
-    excel_file_path = 'TestFile.xlsx'
 
-    # Read the Excel file using openpyxl
-    workbook = openpyxl.load_workbook(excel_file_path)
-    sheet = workbook.active
+# def excel_to_chart(request):
+#     # Your Excel file path (or it could be uploaded by the user in a form)
+#     excel_file_path = 'TestFile.xlsx'
 
-    # Extract data from the Excel sheet (assumes data starts from row 2)
-    # For example, assume the Excel sheet has columns 'Date' and 'Value'
-    dates = []
-    values = []
+#     # Read the Excel file using openpyxl
+#     workbook = openpyxl.load_workbook(excel_file_path)
+#     sheet = workbook.active
 
-    for row in sheet.iter_rows(min_row=2, values_only=True):
-        dates.append(row[0])  # assuming 'Date' is the first column
-        values.append(row[1])  # assuming 'Value' is the second column
+#     # Extract data from the Excel sheet (assumes data starts from row 2)
+#     # For example, assume the Excel sheet has columns 'Date' and 'Value'
+#     dates = []
+#     values = []
 
-    # Create a chart using matplotlib
-    fig, ax = plt.subplots()
-    ax.plot(dates, values, marker='o', linestyle='-', color='b')
+#     for row in sheet.iter_rows(min_row=2, values_only=True):
+#         dates.append(row[0])  # assuming 'Date' is the first column
+#         values.append(row[1])  # assuming 'Value' is the second column
 
-    ax.set(xlabel='Date', ylabel='Value',
-           title='Example Data Visualization')
+#     # Create a chart using matplotlib
+#     fig, ax = plt.subplots()
+#     ax.plot(dates, values, marker='o', linestyle='-', color='b')
 
-    # Generate the plot and send as response
-    response = HttpResponse(content_type='image/png')
-    canvas = FigureCanvas(fig)
-    canvas.print_png(response)
-    return response
+#     ax.set(xlabel='Date', ylabel='Value',
+#            title='Example Data Visualization')
+
+#     # Generate the plot and send as response
+#     response = HttpResponse(content_type='image/png')
+#     canvas = FigureCanvas(fig)
+#     canvas.print_png(response)
+#     return response
