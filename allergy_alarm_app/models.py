58a8e6fa90c6409b71e234d7c392f9bd53906423
Triangleman1 <spnewman21@gmail.com>
2025-04-15 20:51:51 -0400

    Rewrote custom user class to be an extension of Django's authentication user class. We can now add user-specific data that's protected by and shares info with authentication.

--- allergy_alarm_app/models.py
+++ allergy_alarm_app/models.py
@@ -1,44 +1,54 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class UserIdentification(models.Model):
-    username = models.TextField()
-    #An implicit primary key column is automatically created. All ForeignKeys to this table refer to this column.
+#user.object.userExtension should refer to this
+class UserExtension(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    test = models.TextField(default="Default text")
 
     def __str__(self):
-        return f"{self.username}"
+        return f"{self.user.username}"
+    
+# @receiver(post_save, sender=User)
+# def extend_user(sender, instance, **kwargs):
+#     u = UserExtension(user = instance, test = "test1") 
+#     u.save()
 
 class Temperature(models.Model):
-    userID = models.ForeignKey(UserIdentification, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
     temperature = models.IntegerField()
 
     def __str__(self):
-        return f"{self.datetime}: {self.userID} recorded temperature {self.temperature}"
+        return f"{self.datetime}: {self.user} recorded temperature {self.temperature}"
     
 class HeartRate(models.Model):
-    userID = models.ForeignKey(UserIdentification, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
     ECG = models.IntegerField()
 
     def __str__(self):
-        return f"{self.datetime}: {self.userID} recorded ECG: {self.ECG}"
+        return f"{self.datetime}: {self.user} recorded ECG: {self.ECG}"
     
 class Accelerometer(models.Model):
-    userID = models.ForeignKey(UserIdentification, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
     x = models.IntegerField()
     y = models.IntegerField()
     z = models.IntegerField()
 
     def __str__(self):
-        return f"{self.datetime}: {self.userID} recorded acceleration x: {self.x}, y:{self.y}, z:{self.z}"
+        return f"{self.datetime}: {self.user} recorded acceleration x: {self.x}, y:{self.y}, z:{self.z}"
     
 class Gyroscope(models.Model):
-    userID = models.ForeignKey(UserIdentification, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
     x = models.IntegerField()
     y = models.IntegerField()
     z = models.IntegerField()
 
     def __str__(self):
-        return f"{self.datetime}: {self.userID} recorded rotational acceleration x: {self.x}, y:{self.y}, z:{self.z}"
+        return f"{self.datetime}: {self.user} recorded rotational acceleration x: {self.x}, y:{self.y}, z:{self.z}"
