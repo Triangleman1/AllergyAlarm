aa91a4ef4a2fee2e875a0472b0a7cbe5679dbd13
Triangleman1 <spnewman21@gmail.com>
2025-04-15 20:29:06 -0400

    Added superuser creation to runall

--- runall.py
+++ runall.py
@@ -1,5 +1,9 @@
 import os
+from django.contrib.auth.models import User
+
 os.system("python manage.py collectstatic")
 os.system("python manage.py makemigrations")
 os.system("python manage.py migrate")
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('username', 'fakeemail@gmail.com', 'allergyalarm')
 os.system("python manage.py runserver")
